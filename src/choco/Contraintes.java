c2021df080054f06cf55e788ab7160e8cc12a937
Baptiste <bapt.maillot@gmail.com>
2016-01-11 14:38:48 +0100

    Implementation Resolution

--- src/choco/Contraintes.java
+++ src/choco/Contraintes.java
@@ -11,11 +11,16 @@ import org.chocosolver.solver.variables.VF;
 public class Contraintes {
 	
 	private Probleme aResoudre;
+	private Solver solver;
+	private IntVar[][][]  X;
 	
 	
-	public Contraintes(Probleme aResoudre) {
+	public Contraintes(Probleme aResoudre, Solver solver, IntVar[][][]  X) {
 		super();
 		this.aResoudre = aResoudre;
+		this.solver = solver;
+		this.X=X;
+		
 	}
 
 	public Probleme getaResoudre() {
@@ -25,29 +30,45 @@ public class Contraintes {
 	public void setaResoudre(Probleme aResoudre) {
 		this.aResoudre = aResoudre;
 	}
+	
+	public void setSolver(Solver solver) {
+		this.solver = solver;
+	}
+	
+	public Solver getSolver() {
+		return solver;
+	}
+	
+	public void setX(IntVar[][][] x) {
+		X = x;
+	}
 
-	public Constraint[][][] contrainteHeureFermeture(Solver solver, IntVar[][][] X){
+	public IntVar[][][] getX() {
+		return X;
+	}
+	
+	public Constraint[][][] contrainteHeureFermeture(){
 		Constraint[][][] C2 = new Constraint[this.aResoudre.getnPatients()][][];
 		for(int i=0;i<this.aResoudre.getnPatients();i++){
 			C2[i] = new Constraint[this.aResoudre.getnG_i()[i]][];
 			for (int j = 0; j < this.aResoudre.getnG_i()[i]; j++) {
 				C2[i][j] = new Constraint[this.aResoudre.getnS_ij()[i][j]];
 				for (int k = 0; k < this.aResoudre.getnS_ij()[i][j]; k++) {
-					C2[i][j][k]= IntConstraintFactory.arithm(X[i][j][k], "<=", this.aResoudre.getHFermeture()-this.aResoudre.getL_ijk()[i][j][k]);
+					C2[i][j][k]= IntConstraintFactory.arithm(this.X[i][j][k], "<=", this.aResoudre.getHFermeture()-this.aResoudre.getL_ijk()[i][j][k]);
 				}
 			}
 		}
 		return C2;
 	}
 	
-	public Constraint[][][] contrainteHeureOuverture(Solver solver, IntVar[][][] X){
+	public Constraint[][][] contrainteHeureOuverture(){
 		Constraint[][][] C3 = new Constraint[this.aResoudre.getnPatients()][][];
 		for(int i=0;i<this.aResoudre.getnPatients();i++){
 			C3[i] = new Constraint[this.aResoudre.getnG_i()[i]][];
 			for (int j = 0; j < this.aResoudre.getnG_i()[i]; j++) {
 				C3[i][j] = new Constraint[this.aResoudre.getnS_ij()[i][j]];
 				for (int k = 0; k < this.aResoudre.getnS_ij()[i][j]; k++) {
-					C3[i][j][k]= IntConstraintFactory.arithm(X[i][j][k], ">=", this.aResoudre.getHOuverture());
+					C3[i][j][k]= IntConstraintFactory.arithm(this.X[i][j][k], ">=", this.aResoudre.getHOuverture());
 				}
 			}
 		}
