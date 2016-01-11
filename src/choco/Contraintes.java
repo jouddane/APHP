aa953da8e0b535c8a2786cfc92c1f5f137cc349a
Baptiste <bapt.maillot@gmail.com>
2016-01-11 14:09:05 +0100

    Implementation COntraintes et Resolution

--- src/choco/Contraintes.java
+++ src/choco/Contraintes.java
@@ -10,25 +10,47 @@ import org.chocosolver.solver.variables.VF;
 
 public class Contraintes {
 	
-	public static Constraint[][][] ContrainteHeureFermeture(Solver solver, Probleme p, IntVar[][][] X){
-		
-		int nPatients =p.getnPatients();
-		int[] nG = p.getnG_i();
-		int[][] nS = p.getnS_ij();
-		int nPeriodes = p.getnPeriodes();
-		
-		
-		Constraint[][][] C2 = new Constraint[nPatients][][];
-		for(int i=0;i<nPatients;i++){
-			C2[i] = new Constraint[nG[i]][];
-			for (int j = 0; j < nG[i]; j++) {
-				C2[i][j] = new Constraint[nS[i][j]];
-				for (int k = 0; k < nS[i][j]; k++) {
-					C2[i][j][k]= IntConstraintFactory.arithm(X[i][j][k], "<=", p.getHFermeture()-p.getL_ijk()[i][j][k]);
+	private Probleme aResoudre;
+	
+	
+	public Contraintes(Probleme aResoudre) {
+		super();
+		this.aResoudre = aResoudre;
+	}
+
+	public Probleme getaResoudre() {
+		return aResoudre;
+	}
+
+	public void setaResoudre(Probleme aResoudre) {
+		this.aResoudre = aResoudre;
+	}
+
+	public Constraint[][][] contrainteHeureFermeture(Solver solver, IntVar[][][] X){
+		Constraint[][][] C2 = new Constraint[this.aResoudre.getnPatients()][][];
+		for(int i=0;i<this.aResoudre.getnPatients();i++){
+			C2[i] = new Constraint[this.aResoudre.getnG_i()[i]][];
+			for (int j = 0; j < this.aResoudre.getnG_i()[i]; j++) {
+				C2[i][j] = new Constraint[this.aResoudre.getnS_ij()[i][j]];
+				for (int k = 0; k < this.aResoudre.getnS_ij()[i][j]; k++) {
+					C2[i][j][k]= IntConstraintFactory.arithm(X[i][j][k], "<=", this.aResoudre.getHFermeture()-this.aResoudre.getL_ijk()[i][j][k]);
 				}
 			}
 		}
 		return C2;
 	}
 	
+	public Constraint[][][] contrainteHeureOuverture(Solver solver, IntVar[][][] X){
+		Constraint[][][] C3 = new Constraint[this.aResoudre.getnPatients()][][];
+		for(int i=0;i<this.aResoudre.getnPatients();i++){
+			C3[i] = new Constraint[this.aResoudre.getnG_i()[i]][];
+			for (int j = 0; j < this.aResoudre.getnG_i()[i]; j++) {
+				C3[i][j] = new Constraint[this.aResoudre.getnS_ij()[i][j]];
+				for (int k = 0; k < this.aResoudre.getnS_ij()[i][j]; k++) {
+					C3[i][j][k]= IntConstraintFactory.arithm(X[i][j][k], ">=", this.aResoudre.getHOuverture());
+				}
+			}
+		}
+		return C3;
+	}
 }
--- src/choco/Resolution.java
+++ src/choco/Resolution.java
@@ -3,6 +3,7 @@ package choco;
 import java.Probleme;
 
 import org.chocosolver.solver.Solver;
+import org.chocosolver.solver.constraints.Constraint;
 import org.chocosolver.solver.variables.IntVar;
 import org.chocosolver.solver.variables.VF;
 
@@ -23,27 +24,47 @@ public class Resolution {
 		this.aResoudre = aResoudre;
 	}
 
-	public void resout(Probleme p){
+	public void resout(){
 		
 		//Initialisation du solver
 		Solver solver = new Solver();
 		
-		int nPatients =p.getnPatients();
-		int[] nG = p.getnG_i();
-		int[][] nS = p.getnS_ij();
-		int nPeriodes = p.getnPeriodes();
+		
+		
+		
+		
 				
 		//Initialisation des variables
 		//Xi,j,k : modele a modifier pour coherence avec le code
-		IntVar[][][]  X= new IntVar[nPatients][][];
-		for(int i=0;i<nPatients;i++){
-			X[i] = new IntVar[nG[i]][];
-			for (int j = 0; j < nG[i]; j++) {
-				X[i][j] = new IntVar[nS[i][j]];
-				for (int k = 0; k < nS[i][j]; k++) {
-					X[i][j][k]= VF.enumerated("Xi,j,k", 0, nPeriodes,solver);
+		IntVar[][][]  X= new IntVar[this.aResoudre.getnPatients()][][];
+		for(int i=0; i< this.aResoudre.getnPatients() ;i++){
+			X[i] = new IntVar[this.aResoudre.getnG_i()[i]][];
+			for (int j = 0; j < this.aResoudre.getnG_i()[i]; j++) {
+				X[i][j] = new IntVar[this.aResoudre.getnS_ij()[i][j]];
+				for (int k = 0; k < this.aResoudre.getnS_ij()[i][j]; k++) {
+					X[i][j][k]= VF.enumerated("Xi,j,k", 0, this.aResoudre.getnPeriodes(),solver);
 				}
 			}
 		}
+		
+		// 3. Create and post constraints by using constraint factories
+		Contraintes contraintes = new Contraintes(this.aResoudre);
+		Constraint[][][] contrainteHeureFermeture = contraintes.contrainteHeureFermeture(solver, X);
+		Constraint[][][] contrainteHeureOuverture = contraintes.contrainteHeureOuverture(solver, X);
+		
+		for(int i=0; i< this.aResoudre.getnPatients() ;i++){
+			for (int j = 0; j < this.aResoudre.getnG_i()[i]; j++) {
+				for (int k = 0; k < this.aResoudre.getnS_ij()[i][j]; k++) {
+					solver.post(contrainteHeureFermeture[i][j][k]);
+					solver.post(contrainteHeureOuverture[i][j][k]);
+				}
+			}
+		}
+        // 4. Define the search strategy
+        solver.set(IntStrategyFactory.lexico_LB(x, y));
+        // 5. Launch the resolution process
+        solver.findSolution();
+        //6. Print search statistics
+        Chatterbox.printStatistics(solver);
 	}
 }
