9923746415071aa9e72abf5f7cada84800efdeb4
mguldner <mathieuguldner.emn@gmail.com>
2016-01-11 15:04:15 +0100

    Add of C4

--- src/choco/Contraintes.java
+++ src/choco/Contraintes.java
@@ -9,10 +9,10 @@ import org.chocosolver.solver.variables.IntVar;
 import org.chocosolver.solver.variables.VF;
 
 public class Contraintes {
-	
+
 	private Probleme aResoudre;
-	
-	
+
+
 	public Contraintes(Probleme aResoudre) {
 		super();
 		this.aResoudre = aResoudre;
@@ -39,7 +39,7 @@ public class Contraintes {
 		}
 		return C2;
 	}
-	
+
 	public Constraint[][][] contrainteHeureOuverture(Solver solver, IntVar[][][] X){
 		Constraint[][][] C3 = new Constraint[this.aResoudre.getnPatients()][][];
 		for(int i=0;i<this.aResoudre.getnPatients();i++){
@@ -52,5 +52,21 @@ public class Contraintes {
 			}
 		}
 		return C3;
+	}
+
+	public Constraint[][][] contraintePrecedenceGroupe(Solver solver, IntVar[][][] X){
+		Constraint[][][] C4 = new Constraint[this.aResoudre.getnPatients()][][];
+		for(int i=0; i<this.aResoudre.getnPatients(); i++){
+			C4[i] = new Constraint[this.aResoudre.getnG_i()[i]-1][];
+			for(int j=0; j<this.aResoudre.getnG_i()[i]; j++){
+				C4[i][j] = new Constraint[this.aResoudre.getnS_ij()[i][j]];
+				for (int k=0; k<this.aResoudre.getnS_ij()[i][j]; k++) {
+					for(int u=0; u<this.aResoudre.getnS_ij()[i][j+1]; u++){
+						C4[i][j][k] = IntConstraintFactory.arithm(X[i][j][k], "<=", X[i][j+1][u], "-", this.aResoudre.getL_ijk()[i][j][i]);
+					}
+				}
+			}
+		}
+		return null;
 	}
 }
