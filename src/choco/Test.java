cec37629fbdad37b3040b168585e42e528e6ec18
mguldner <mathieuguldner.emn@gmail.com>
2016-01-25 17:22:33 +0100

    changes Test

--- src/choco/Test.java
+++ src/choco/Test.java
@@ -28,47 +28,49 @@ public class Test {
 		donnees.setHFermeture(20*60);
 		donnees.setHOuverture(6*60);
 		
+		//Ajout ressources matérielles
 		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "HDJ Obesite", 10));
 		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Box", 10));
 		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Box prelevement", 10));
 		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Hors HDJ", 10));
 		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Bureau CS", 10));
 		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Box soin", 10));
-		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "salle pansement", 10));
-		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "explorations fonctionnelles", 10));
-		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "pièce isolée avec fauteuil", 10));
-		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "bureau sommeil", 10));
-		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "salle etp groupe", 10));
-		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "salle avec lit", 10));
+		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Salle pansement", 10));
+		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Explorations fonctionnelles", 10));
+		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Pièce isolée avec fauteuil", 10));
+		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Bureau sommeil", 10));
+		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Salle etp groupe", 10));
+		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Salle avec lit", 10));
 		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "HDJ chimio", 10));
-		
+
+		//Ajout ressources humaines
 		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "IDE Obesité", 10));
 		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "IDE", 10));
-		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "nutritioniste", 10));
+		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Nutritioniste", 10));
 		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Diététicien", 10));
 		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Psychologue", 10));
 		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Interne obésité", 10));
 		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Médecin hépato", 10));
-		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "externe", 10));
+		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Externe", 10));
 		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "IDE cardio", 10));
-		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "cardiologue", 10));
-		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "orthoptiste", 10));
-		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "diabétologue", 10));
-		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "podologue", 10));
+		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Cardiologue", 10));
+		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Orthoptiste", 10));
+		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Diabétologue", 10));
+		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Podologue", 10));
 		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "IDE pansement", 10));
-		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "médecin", 10));
+		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Médecin", 10));
 		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "IDE pompe insuline", 10));
 		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "IDE insulinothérapie", 10));
-		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "médecin sommeil", 10));
+		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Médecin sommeil", 10));
 		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "IDE sommeil", 10));
-		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "prestataire", 10));
+		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Prestataire", 10));
 		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "IDE chimio", 10));
 		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Generaliste", 10));
 		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Soignant", 10));
 		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Selon profil(P20)", 10));
 		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Kine", 10));
-		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "agent accueil", 10));
-		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "neurologue", 10));
+		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Agent accueil", 10));
+		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Neurologue", 10));
 		donnees.ajoutRessource(Ressource.RessourceConstante(nPeriodes, "Neuropsy", 10));
 		
 		int nRessources = donnees.getRessources().length;
@@ -128,8 +130,7 @@ public class Test {
 		listRessourceCapacite10.add(new CoupleStringInt("IDE obesite", 1));	
 		listRessourceCapacite10.add(new CoupleStringInt("HDJ obesite", 1));	 	 	 	 	 	
 		Soin RDVParamedical = Soin.creerSoin(donnees, listRessourceCapacite10, "RDV paramedical", 20);
-		
-		
+			
 		ArrayList<CoupleStringInt> listRessourceCapacite12 = new ArrayList<>();	 	 	 	 	 	 	 	
 		Soin TOGD = Soin.creerSoin(donnees, listRessourceCapacite12, "TOGD", 20);
 	
