9a78cea7a3af0cb40d6de99d9095dd24e69a5f0f
mguldner <mathieuguldner.emn@gmail.com>
2016-02-15 11:47:02 +0100

    Add javadoc

--- src/dev/SoinAffecte.java
+++ src/dev/SoinAffecte.java
@@ -1,21 +1,37 @@
 package dev;
 
-
+/**
+ * Classe representant un soin affecte a un patient
+ * 
+ */
 
 public class SoinAffecte extends Soin{
 
+    /**
+     * L'heure de debut du soin en nombre de periodes
+     */
 	private int debut;
+	
+	/**
+	 * Booleen permettant de savoir si le soin est deja realise
+	 */
 	private boolean realise;
+	
+	/**
+	 * Booleen permetant de savoir si le soin est en cours de realisation
+	 */
 	private boolean enCours;
 	
-	
+	/**
+	 * Constructeur d'un soin affecte a partir d'un soin existant
+	 * @param s le soin existant a affecter a un patient
+	 * @param debut l'heure de debut de ce soin en nombre de periodes
+	 */
 	public SoinAffecte(Soin s, int debut){
 		super(s.getDuree(), s.getRessourcesNecessaires(), s.nom);
 		this.debut = debut;
 		this.realise = false;
 		this.enCours = false;
-		
-		
 	}
 	
 	public int getDebut(){
